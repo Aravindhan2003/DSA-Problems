0c55fbd27a25db54d934bfe3a45feada2763496d
Aravindhan2003 <aravindhanofficial2003@gmail.com>
2024-04-04 14:30:06 +0530

    maximum profit completed

--- Arrays/maxProfit.py
+++ Arrays/maxProfit.py
@@ -12,31 +12,22 @@
 # and selling it at minute 2 when its price is Rs. 150.
 # So, the output will be 148.
 
-arr = [2, 100, 150, 120, 180]
+def maximumProfit(arr):
+    min_price = arr[0]
+    max_profit = 0
 
-left = 0
-right = 1
-max_profit = 0
+    for i in range(1, len(arr)):
+        if arr[i] < min_price:
+            min_price = arr[i]
+        
+        else:
+            profit = arr[i] - min_price
+            if max_profit < profit:
+                max_profit = profit
 
-for i in range(len(arr)):
-    for j in range(i+1, len(arr)):
-        if arr[j] - arr[i] > max_profit:
-            max_profit = arr[j] - arr[i]
+    return max_profit
 
-print(max_profit)
+arr = [2, 100, 1, 150, 120, 200]
+maximumProfit(arr)
 
-# while left < right:
-#     if arr[right] - arr[left] > max_profit:
-#         max_profit = arr[right] - arr[left]
-    
-#     # else:
-#     #     left += 1
-#     print(max_profit)
-#     right += 1
-
-# for i in arr:
-#     if arr[right] - i > max_profit:
-#         max_profit = arr[right] - i
-
-#     right += 1
     
