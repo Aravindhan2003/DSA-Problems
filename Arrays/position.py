276317af354ebcf967d38071574b70256ce83c8d
Aravindhan2003 <aravindhanofficial2003@gmail.com>
2024-04-16 22:28:46 +0530

    started range of position

---
+++ Arrays/position.py
@@ -0,0 +1,34 @@
+# Given an array of integers nums sorted in non-decreasing order, 
+# find the starting and ending position of a given target value.
+
+# If target is not found in the array, return [-1, -1].
+
+# You must write an algorithm with O(log n) runtime complexity.
+
+def findPosition(nums, target):
+    if target not in nums:
+        return [-1, -1]
+
+    else:
+        start = 0
+        end = len(nums)-1
+
+        while start <= end:
+            mid = (start+end)//2
+            if nums[mid] < target:
+                start = mid+1
+
+            elif nums[mid] > target:
+                end = mid-1
+
+            else:
+                # start = mid
+                if nums[end] > target:
+                    end -= 1
+                else:
+                    return [mid, end]
+
+nums = [1]
+target = 1
+print(findPosition(nums, target))
+
